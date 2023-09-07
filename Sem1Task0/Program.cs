00359344b4a97ae2962bdbfc6b866c3980ee10c1
Nessime <nessime-89@ya.ru>
2023-09-07 23:01:54 +0500

    семинар 1

---
+++ Sem1Task0/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача №0
+// программа выдает квадрат исходного числа
+
+Console.WriteLine("Введите число");
+//считываем данные с консоли
+string? inputNum = Console.ReadLine();
+//проверяем, чтобы данные были не пустыми
+if(inputNum!=null)
+{
+    //парсим введенное число
+//    int num = int.Parse(inputNum);
+    //находим квадрат числа
+//    int res = num*num;
+    //выводим данные в консоль
+//    Console.WriteLine(res);
+
+    Console.WriteLine("Квадрат числа: "+(int)Math.Pow(int.Parse(inputNum),2));
+}
+
+
