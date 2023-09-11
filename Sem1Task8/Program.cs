14a7bde456bdacf3ff8a6ed5d8cebc9339b35cc4
Nessime <nessime-89@ya.ru>
2023-09-11 22:30:59 +0500

    Задание 8

--- Sem1Task8/Program.cs
+++ Sem1Task8/Program.cs
@@ -2,3 +2,14 @@
 
 //Напишите программу, которая на вход принимает число (N), а на выходе показывает
 //все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число больше 1");
+int NumN = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= NumN; i++)
+{
+    if (i % 2 == 0)
+        {
+        System.Console.WriteLine(i + ", ");
+        }
+}
