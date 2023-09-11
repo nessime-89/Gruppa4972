bbd5075d46d706176164ea803b73b4862a1b87d8
Nessime <nessime-89@ya.ru>
2023-09-11 23:05:46 +0500

    Добавление комментариев

--- Sem1Task4/Program.cs
+++ Sem1Task4/Program.cs
@@ -2,6 +2,7 @@
 
 //  Задача №4
 
+//ввести 3 числа
 Console.WriteLine("Введите число 1");
 int num_1 = int.Parse(Console.ReadLine());
 
@@ -11,26 +12,33 @@ int num_2 = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите число 3");
 int num_3 = int.Parse(Console.ReadLine());
 
-
+//проверка какое число больше. число 1 > числа 2
 if (num_1 > num_2)
 {
+//число 1 > числа 2
+if (num_1 > num_2)
+//число 1 > числа 3
     if (num_1 > num_3)
     {
         Console.WriteLine("Максимальное число 1");
     }
+
     else
+//число 1 < числа 3
     {
         Console.WriteLine("Максимальное число 3");
     }
 }
+
 else
 {
+//число 1 < числа 2
     if (num_2 > num_3)
-    {
+    {//число 3 < числа 2
         Console.WriteLine("Максимальное число 2");
     }
     else
-    {
+    {//число 2 < числа 3
         Console.WriteLine("Максимальное число 3");
     }
 }
