e0c7ba59dc939ad8cfedcfd5d6b20d7de983ad4c
Nessime <nessime-89@ya.ru>
2023-09-11 22:05:36 +0500

    Задание6

--- Sem1Task6/Program.cs
+++ Sem1Task6/Program.cs
@@ -2,3 +2,14 @@
 
 //Напишите программу, которая на вход принимает число и выдаёт, является ли число
 //чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 0)
+    {
+    System.Console.WriteLine("Число: " + a + " четное");
+    }
+else 
+    {
+    System.Console.WriteLine("Число: " + a + " нечетное");
+    }
