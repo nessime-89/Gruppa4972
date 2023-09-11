722ed5001518a24c23b248d68b5904a3d8d3e01b
Nessime <nessime-89@ya.ru>
2023-09-11 21:55:56 +0500

    задание 5

--- Sem1Task5/Program.cs
+++ Sem1Task5/Program.cs
@@ -4,5 +4,14 @@
 //одно число (N), а на выходе показывает все целые
 //числа в промежутке от -N до N.
 
+Console.WriteLine("Введите целое число бельше 0");
+int NumN = int.Parse(Console.ReadLine());
+int evNumN = (-1)*NumN;
+while (evNumN < NumN)
+{
+    Console.Write (evNumN + ", ");
+    evNumN++;
+}
 
+Console.WriteLine (NumN);
 
