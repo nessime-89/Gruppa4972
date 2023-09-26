d875fec763c5a4fab1782127a72ba0a17777435e
Nessime <nessime-89@ya.ru>
2023-09-26 22:56:52 +0500

    10

--- Sem2Task10/Program.cs
+++ Sem2Task10/Program.cs
@@ -3,15 +3,15 @@
 //Напишите программу, которая принимает на вход трёхзначное число и на выходе
 //показывает вторую цифру этого числа. 
 
+// сгенерировать число и вывести его на экран
+int num = new System.Random().Next(100, 1000);
+System.Console.WriteLine(num);
+// перевод в строку и вывод 2го числа
+char[] digitChar = num.ToString().ToCharArray();
+System.Console.Write(digitChar[1]);
 
 
-    // сгенерировать число и вывести его на экран
-    System.Random numberGenerator = new System.Random();
-    int number = numberGenerator.Next(100, 1000);
-    Console.WriteLine(number);
 
-    int first = number / 100;
-    int last = number % 10;
 
-    number = first*10 + last;
-    Console.WriteLine(number);
+
+    
--- Sem2Task11/Program.cs
+++ Sem2Task11/Program.cs
@@ -2,3 +2,14 @@
 
 //Напишите программу, которая выводит случайное
 //трёхзначное число и удаляет вторую цифру этого числа. 
+
+// сгенерировать число и вывести его на экран
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(100, 1000);
+    Console.WriteLine(number);
+//найти 1 и 3 число
+    int first = number / 100;
+    int last = number % 10;
+// сложить 1 число и 3 число
+    number = first*10 + last;
+    Console.WriteLine(number);
