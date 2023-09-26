2772fe2b578336318e119e0d879cfad8e5627ef9
Nessime <nessime-89@ya.ru>
2023-09-26 23:52:57 +0500

    сохр

---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход
+//два числа и проверяет, является ли одно число квадратом другого.
+
+Console.WriteLine("Введите первое число");
+int num1 = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine("Введите второе число");
+int num2 = int.Parse(Console.ReadLine()??"0");
+
+
