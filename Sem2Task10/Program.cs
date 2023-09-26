075559e95c0864bc0fd3528789f4144448537e28
Nessime <nessime-89@ya.ru>
2023-09-26 22:42:37 +0500

    11

--- Sem2Task10/Program.cs
+++ Sem2Task10/Program.cs
@@ -2,3 +2,16 @@
 
 //Напишите программу, которая принимает на вход трёхзначное число и на выходе
 //показывает вторую цифру этого числа. 
+
+
+
+    // сгенерировать число и вывести его на экран
+    System.Random numberGenerator = new System.Random();
+    int number = numberGenerator.Next(100, 1000);
+    Console.WriteLine(number);
+
+    int first = number / 100;
+    int last = number % 10;
+
+    number = first*10 + last;
+    Console.WriteLine(number);
