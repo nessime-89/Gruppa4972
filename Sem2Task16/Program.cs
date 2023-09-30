8a35ad3406ea0615cc30fd436076db9c98ed6c0a
Nessime <nessime-89@ya.ru>
2023-09-30 16:00:22 +0500

    2 семинар дз

--- Sem2Task16/Program.cs
+++ Sem2Task16/Program.cs
@@ -1,10 +1,13 @@
 ﻿// Напишите программу, которая принимает на вход
 //два числа и проверяет, является ли одно число квадратом другого.
 
+
+//ввести числа
 Console.WriteLine("Введите первое число");
 int num1 = int.Parse(Console.ReadLine()??"0");
 
 Console.WriteLine("Введите второе число");
 int num2 = int.Parse(Console.ReadLine()??"0");
 
-
+//проверяет, является ли одно число квадратом другого
+Console.WriteLine( num1*num1 == num2 || num2*num2 == num1 ? "да"  : "нет" );
