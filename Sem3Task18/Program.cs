befdc4b05d7d152a452cbdee752e6ad8977fbbd9
Nessime <nessime-89@ya.ru>
2023-10-03 21:48:44 +0500

    18

--- Sem3Task18/Program.cs
+++ Sem3Task18/Program.cs
@@ -3,3 +3,30 @@
 //Напишите программу, которая по заданному номеру
 //четверти, показывает диапазон возможных
 //координат точек в этой четверти (x и y).
+
+// введение № четверти
+int getQuarter()
+{
+    Console.Write("Введите номер четверти: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// //Вводим данные
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     int num = int.Parse(Console.ReadLine() ?? "0");
+//     return num;
+// }
+
+string pointsIdent(int quarterNum)
+{
+    if (quarterNum == 1) return "Возможные значения: X > 0, Y > 0";
+    if (quarterNum == 2) return "Возможные значения: X < 0, Y > 0";
+    if (quarterNum == 3) return "Возможные значения: X < 0, Y < 0";
+    if (quarterNum == 4) return "Возможные значения: X > 0, Y < 0";
+    return "Неверное значение";
+}
+
+int quarterNum = getQuarter();
+Console.WriteLine(pointsIdent(quarterNum));
